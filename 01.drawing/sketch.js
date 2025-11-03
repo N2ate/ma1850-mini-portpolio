56c41f7fabda8e782f22c7ad5fad038301fecd3a
N2ate <wanewau@gmail.com>
2025-11-03 19:55:49 +0000

    Add files via upload

---
+++ 01.drawing/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+ // eyes
+  fill(254,0,0)
+  circle(50,50,50);
+  circle (200,50,50);
+ 
+ // mouth
+  fill (0,255,0,255)
+  ellipse (150,150,200,20)
+}
